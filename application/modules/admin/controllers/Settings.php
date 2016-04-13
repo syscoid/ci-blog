a3cb1c2308000314771520d4fd4c50989bd2ff4d
syscoid <syscoid@gmail.com>
2016-04-13 15:07:16 +0700

    Update Settings.php
    change Admin_controller to MY_Controller

--- application/modules/admin/controllers/Settings.php
+++ application/modules/admin/controllers/Settings.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Settings extends Admin_Controller {
+class Settings extends MY_Controller {
 
 	public function __construct(){
 		parent::__construct();
